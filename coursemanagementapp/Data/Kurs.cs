6a4b48f77fbf5b4442ff50bb3902b8b32dac989c
Şahin Karaoğlu <76259114+sahinkaraoglu@users.noreply.github.com>
2024-01-29 19:54:28 +0300

    Name Edit

--- coursemanagementapp/Data/Kurs.cs
+++ coursemanagementapp/Data/Kurs.cs
@@ -4,7 +4,6 @@ namespace coursemanagementapp.Data
        public class Kurs
        {
               public int KursId { get; set; }
-
               public string? Baslik { get; set; }
               public int OgretmenId { get; set; }
               public Ogretmen Ogretmen { get; set; } = null!;
--- coursemanagementapp/Data/Ogrenci.cs
+++ coursemanagementapp/Data/Ogrenci.cs
@@ -5,12 +5,15 @@ namespace coursemanagementapp.Data
     public class Ogrenci
     {
         [Key]
+        [Display(Name = "Öğrenci Id")]
         public int OgrenciId { get; set; }
+
+        [Display(Name = "Ad")]
         public string? OgrenciAd { get; set; }
+        [Display(Name = "Soyad")]
         public string? OgrenciSoyad { get; set; }
 
         [Display(Name = "Öğrenci Ad Soyad")]
-
         public string? AdSoyad
         {
             get
